bb641427995177355086639586da3df89f839d1c
Pedro Jorge Oliveira Braga <pjobraga98@gmail.com>
2023-12-21 13:53:48 -0300

    Segundo capitulo
    Continuação da aula.

--- start/routes.ts
+++ start/routes.ts
@@ -1,25 +1,36 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Route from '@ioc:Adonis/Core/Route'
+import User from 'App/Models/User'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  User.create({
+    email: "admin@email.com",
+    password: "123456",
+    tipo: "admin"
+  })
 })
+
+Route.post('/gettoken', async ({request, response, auth} : HttpContextContract) => {
+  const email = request.input("email");
+  const password = request.input("password");
+
+  const user = await User.findBy("email", email);
+
+  if(user == null){
+    return response.notFound("User nao encontrado")
+  }
+
+  const token = await auth.use('api').attempt(email, password);
+  return response.ok(token);
+});
+
+Route.get('/auth', async ({auth, response} : HttpContextContract) => {
+  const userAuth = await auth.use('api').authenticate();
+
+  if(userAuth){
+    return response.ok(userAuth);
+  } else {
+    return response.unauthorized("User nao autenticado");
+  }
+});
+
