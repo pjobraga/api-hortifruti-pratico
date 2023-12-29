fa0624e9243b925c3db9dfc822b932f2a0483029
Pedro Jorge Oliveira Braga <pjobraga98@gmail.com>
2023-12-29 15:58:07 -0300

    Criando usuarios
    Criando os 3 tipos de Users em nosso sistema;

--- start/routes.ts
+++ start/routes.ts
@@ -1,30 +1,9 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Route from '@ioc:Adonis/Core/Route'
-import User from 'App/Models/User'
-
+ 
 Route.get('/', async () => {
-  User.create({
-    email: "admin@email.com",
-    password: "123456",
-    tipo: "admin"
-  })
+  return{
+    hortifruti: "pratico",
+  }
 })
 
-Route.post('/gettoken', async ({request, response, auth} : HttpContextContract) => {
-  const email = request.input("email");
-  const password = request.input("password");
-
-  const user = await User.findBy("email", email);
-
-  if(user == null){
-    return response.notFound("User nao encontrado")
-  }
-
-  const token = await auth.use('api').attempt(email, password);
-  return response.ok(token);
-});
-
-Route.get('/auth', async ({ response } : HttpContextContract) => {
-    return response.ok("Somente usuarios autenticados podem acessar");
-}).middleware("auth");
 
