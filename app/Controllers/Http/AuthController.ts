43adca370bc8856d867956e6579a562089ad4d74
Pedro Jorge Oliveira Braga <pjobraga98@gmail.com>
2024-01-03 13:47:59 -0300

    Auth
    Completando o auth

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,3 +1,31 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
-export default class AuthController {}
+export default class AuthController {
+    public async login({ auth , request, response }: HttpContextContract){
+        const email = request.input('email')
+        const password = request.input('password')
+
+        try{
+            const user = await User.findByOrFail("email", email)
+
+            let expira;
+            switch (user.tipo) {
+                case "clientes":
+                    expira = "30days";
+                    break;
+                case "estabelecimentos":
+                    expira = "7days";
+                    break
+                case "admins":
+                    expira = "1days";
+                    break;
+                default:
+                    expira = "30days"
+                    break;
+            }
+        } catch {
+            return response.badRequest("Invalid Credentials")
+        }
+    }
+}
